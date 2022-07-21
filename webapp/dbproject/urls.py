f591fc25f5fa32d3972c8d3fa54eef590413141f
VadymKuntso1 <davidenkodavid50@gmail.com>
2022-07-21 22:30:36 +0300

    21/7 21:30

--- webapp/dbproject/urls.py
+++ webapp/dbproject/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='home'),
+    path('new', views.new, name="new")
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
